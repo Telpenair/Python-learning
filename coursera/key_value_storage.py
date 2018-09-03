2e3b101bdaf6ad457a7f6948e7d8e7f1e8c7b0dc
Konstantin Alifanov <41705612+Telpenair@users.noreply.github.com>
2018-09-04 00:39:29 +0300

    Make some dir sorting

---
+++ coursera/key_value_storage.py
@@ -0,0 +1,40 @@
+import os
+import tempfile
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--key", help="get value if only, puts key & val if with --val")
+parser.add_argument("--val", help="puts key & val")
+args = parser.parse_args()
+
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+if not os.path.exists(storage_path):
+    with open(storage_path, 'w') as f:
+        f.write(json.dumps({}))
+
+if(args.key and args.val): 
+    with open(storage_path, 'r') as f:
+        
+        json_temp = f.read()
+        dict_temp = json.loads(json_temp)
+        if args.key in dict_temp:
+            dict_temp[args.key].append(args.val)
+        else:
+            dict_temp[args.key] = [args.val]
+        json_temp = json.dumps(dict_temp)
+
+    with open(storage_path, 'w') as f:
+
+        f.write(json_temp)
+
+elif(args.key):
+    with open(storage_path, 'r') as f:
+        
+        json_temp = f.read()
+        dict_temp = json.loads(json_temp)
+        answer = dict_temp.get(args.key, None)
+        if answer:
+            print(', '.join(answer))
+        else:
+            print(answer)
--- decorator_to_json.py
+++
@@ -1,9 +0,0 @@
-import functools
-import json
-
-def to_json(func):
-    @functools.wraps(func)
-    def wrapped(*args, **kwargs):
-        result = json.dumps(func(*args, **kwargs))
-        return result
-    return wrapped
